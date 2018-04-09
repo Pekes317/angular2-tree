167183c8d559d84a21a3e159967159b3548890a8
Pekes317 <El_Dante565@yahoo.com>
2018-04-08 22:11:46 -0500

    published package

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: [
+    // Specify scss files
+    './tree-lib/src/styles/main.scss'
+  ],
+  output: {	},
+  module: {
+    // Add loader
+    rules: [{
+			test: /\.scss$/,
+			loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+		}]
+  },
+  plugins: [
+    // Specify output file name and path
+    new ExtractTextPlugin({
+      filename: './dist/angular2-tree/style.css'
+    })
+  ]
+};
