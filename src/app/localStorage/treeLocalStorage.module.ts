7f745d4a2b04c5e15b4d0f5efe39f7b001719a5e
Pekes317 <El_Dante565@yahoo.com>
2018-05-10 07:32:57 -0500

    code correction

--- src/app/localStorage/treeLocalStorage.module.ts
+++ src/app/localStorage/treeLocalStorage.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { TreeModule } from '../../../tree-lib/src/tree.module';
 import { TreeLocalStorageNodeService } from './treeLocalStorage.service';
 
@@ -9,7 +9,7 @@ import { TreeLocalStorageNodeService } from './treeLocalStorage.service';
   exports: [],
   imports: [
     BrowserModule,
-    HttpModule,
+    HttpClientModule,
     TreeModule
   ],
   providers: [TreeLocalStorageNodeService]
